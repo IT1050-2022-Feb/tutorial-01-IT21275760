dfccb1347acec4d79a47fca825a20d648b1e059c
IT21275760 <99953424+IT21275760@users.noreply.github.com>
2022-02-23 16:25:50 +0000

    finished

--- Tute01.c
+++ Tute01.c
@@ -4,7 +4,20 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+  int mark1,mark2;
+	int total=0;
+	double average;
+	printf("subject 01 :");
+	scanf("%d",&mark1);
+	printf("subject 02 :");
+	scanf("%d",&mark2);
+	
+	total=mark1+mark2;
+	average=(total/2);
+	printf("\nAverage is :%.2f",average);
+
   
   return 0;
 }
