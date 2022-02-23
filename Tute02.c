1e4bcf9516d04ad703ab5bb6c70874fd967596d6
IT21275760 <99953424+IT21275760@users.noreply.github.com>
2022-02-23 16:28:49 +0000

    Exercise02

--- Tute02.c
+++ Tute02.c
@@ -16,9 +16,27 @@ Amount = 20 x 50 = 1000
 Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
-#include <stdio.h>
-
-int main() {
-  
-  return 0;
+#include<stdio.h>
+int main()
+{
+	int distance,N_distance=0;
+	int rate,N_rate=0;
+	printf("Amount to be paid for a rented vehicle\n\n");
+	printf("Enter the  travelled distance :");
+	scanf("%d",&distance);
+	if(distance<=30)
+	{
+		rate=distance*50;
+		printf("Amount is =%d",rate);
+	}
+	else if(distance>30)
+	{
+		rate=30*50;
+		N_distance=distance-30;
+		N_rate=rate+(N_distance*40);
+		
+		printf("\nAmount is =%d",N_rate);
+	}
+	return 0;
 }
+
